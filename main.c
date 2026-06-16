d34070379b05f594507b7fbfa157a79283cf774b
Yeonwoo Bae <yeonwoob@uci.edu>
2026-06-15 17:56:51 -0700

    created child process to execute the tokenized input

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 void tokenize(char arr[], char *args[]);
 
 int main()
 {
-    char buff[100];
+    char *buff = NULL;
+    size_t size = 0;
+    ssize_t read;
 
     while(1)
     {
@@ -14,27 +19,46 @@ int main()
 
         //when fget runs and the user types something, the input stream attaches a \n at the end
         //a C string is just an array of characters that has a \0 at the end
-        if(fgets(buff, sizeof(buff), stdin) == NULL)
+        read = getline(&buff, &size, stdin);
+        if(read == -1)
         {
             break;
         }
 
         buff[strcspn(buff, "\n")] = '\0';
 
-        if(strcmp(buff, "exit") == 0)
+        if(buff[0] == '\0')
         {
-            break;
+            continue;
         }
 
         char *array[100];
         tokenize(buff, array);
 
-        for(int i = 0; array[i] != NULL; i++)
+        if(array[0] != NULL && strcmp(array[0], "exit") == 0)
         {
-            printf("arg[%d] = %s\n", i, array[i]);
+            break;
         }
 
+        pid_t p = fork();
+        if (p == 0)
+        {
+            execvp(array[0], array);
+
+            perror("execvp failed");
+            exit(1);
+        }
+        else if (p < 0)
+        {
+            perror("fork failed");
+        }
+        else
+        {
+            waitpid(p, NULL, 0);
+        }
     }
+
+    free(buff);
 
     return 0;
 }
